1bed1cbbfaec01d9d04004a28be2dd6c74453310
shtigran <shakhbekyantigran@gmail.com>
2017-02-08 01:01:11 +0400

    Well done! Pictures well downloaded. Continue to all file types

--- FileDownloader/FileDownloader/Program.cs
+++ FileDownloader/FileDownloader/Program.cs
@@ -27,6 +27,7 @@ namespace FileDownloader
             {   // Creating strings for operations
                 string all = string.Empty;
                 string htmlCode = string.Empty;
+                string path1 = string.Empty;
 
                 using (WebClient client = new WebClient()) // WebClient class inherits IDisposable 
                 {
@@ -36,6 +37,7 @@ namespace FileDownloader
                     Console.WriteLine("-------------");
                     Console.WriteLine("\nThere are the following files: ");
                     string[] split = all.Split(new Char[] { '\n' });
+                    if (path == "https://mail.ru/") split[12] = split[11];
                     Console.WriteLine("-------------");
                    
                     string dir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
@@ -46,16 +48,18 @@ namespace FileDownloader
                     {
                         WebClient client1;
                         client1 = new WebClient();
-                        Uri uri = new Uri(path + item);
+                        path1 = path + item;
+                        Uri uri = new Uri(path1);
 
                         if (item.Contains(".com") || item.Contains(".ru") || item.Contains(".net"))
 
                         {
-                            Console.WriteLine(item);
                             client1 = new WebClient();
-                            uri = new Uri("http:"+item);
+                            path1 = "http:" + item;
+                            uri = new Uri(path1);
                         }
 
+                        Console.WriteLine(item);
 
                         if (item.Contains(".jpg") || item.Contains(".png") || item.Contains(".svg"))
                         {
@@ -67,11 +71,17 @@ namespace FileDownloader
                             { client1.DownloadFile(uri, $"{dir}\\Images\\Picture{flag}.jpg");
                                 flag++;
                             }
-
-                            if (item.Contains(".png"))
+                            if (item.Contains(".png") )
                             { client1.DownloadFile(uri, $"{dir}\\Images\\Picture{flag}.png");
                                 flag++;
                             }
+                            if (item.Contains(".svg"))
+                            {
+                                client1.DownloadFile(uri, $"{dir}\\Images\\Picture{flag}.svg");
+                                flag++;
+                            }
+
+
 
                             // //    if (item.Contains(".svg"))
                             // //    { client1.DownloadFileAsync(uri, $"{dir}\\Images\\Picture{flag}.svg"); }
