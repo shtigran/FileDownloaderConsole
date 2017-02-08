763d1d9b0caf1e0e1e4e4000b9a57c2028519772
shtigran <shakhbekyantigran@gmail.com>
2017-02-08 21:55:45 +0400

    Correcting code

--- FileDownloader/FileDownloader/Program.cs
+++ FileDownloader/FileDownloader/Program.cs
@@ -15,105 +15,81 @@ namespace FileDownloader
         {
 
             // Welcome message
-            Console.WriteLine("\n    |Welcome Website Files Downloader|");
-            Console.Write("\nPlease enter the Website path: ");
+            Console.WriteLine("\n ***|Welcome Website Files Downloader|***");
+            Console.Write("\n     Please enter the Website path: ");
             string path = Console.ReadLine();
 
             // Checking URL validity          
             if (!Uri.IsWellFormedUriString(path, UriKind.RelativeOrAbsolute))
-                { throw new UriFormatException("Wrong URL!!!"); }
+            { throw new UriFormatException("Wrong URL!!!"); }
 
 
-              // Creating strings for operations
-                string all = string.Empty;
-                string htmlCode = string.Empty;
-                string path1 = string.Empty;
+            // Creating strings for operations
+            string all = string.Empty;
+            string htmlCode = string.Empty;
+            string path1 = string.Empty;
 
-                using (WebClient client = new WebClient()) // WebClient class inherits IDisposable 
+            using (WebClient client = new WebClient()) // WebClient class inherits IDisposable 
+            {
+                htmlCode = client.DownloadString(path);
+                all = showMatch(htmlCode, @"([/:.@_a-zA-Z0-9\-]+?)\.(jpg|svg|png)");
+
+                Console.WriteLine("\n|------------------------------|");
+                Console.WriteLine("|There are the following files:| ");
+                string[] split = all.Split(new Char[] { '\n' });
+                if (path == "https://mail.ru/") split[12] = split[11];
+
+                Console.WriteLine("|------------------------------|\n");
+
+                string dir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); // For download direction
+
+
+                int flag = 0;
+                foreach (var item in split)
                 {
-                    htmlCode = client.DownloadString(path);
-                    all = showMatch(htmlCode, @"([/:.@_a-zA-Z0-9\-]+?)\.(jpg|svg|png)");
-                 
+                    path1 = path + item;
+                    Uri uri = new Uri(path1);
 
-                    Console.WriteLine("-------------");
-                    Console.WriteLine("\nThere are the following files: ");
-                    string[] split = all.Split(new Char[] { '\n' });
-                    if (path == "https://mail.ru/") split[12] = split[11];
+                    if (item.Contains(".com") || item.Contains(".ru") || item.Contains(".net"))
 
-                    Console.WriteLine("-------------");
-                   
-                    string dir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-
-
-                    int flag = 0;
-                    foreach (var item in split)
                     {
-                        Console.WriteLine(item);
-                        //path1 = path + item;
-                        //Uri uri = new Uri(path1);
-                        
-                        //if (item.Contains(".com") || item.Contains(".ru") || item.Contains(".net"))
+                        path1 = "http:" + item;
+                        uri = new Uri(path1);
+                    }
 
-                        //{
-                        //    path1 = "http:" + item;
-                        //    uri = new Uri(path1);
-                        //}
+                    
 
-                        //Console.WriteLine(item);
-
-                        //if (item.Contains(".jpg") || item.Contains(".png") || item.Contains(".svg"))
-                        //{
-                        //    if (!Directory.Exists(dir + "\\Images"))
-                        //        Directory.CreateDirectory(dir + "\\Images");
+                    if (item.Contains(".jpg") || item.Contains(".png") || item.Contains(".svg"))
+                    {
+                        if (!Directory.Exists(dir + "\\Images"))
+                            Directory.CreateDirectory(dir + "\\Images");
 
 
-                        //    if (item.Contains(".jpg"))
-                        //    {
-                        //        flag++;
-                        //        client.DownloadFile(uri, $"{dir}\\Images\\Picture{flag}.jpg");
-                                
-                        //    }
-                        //    if (item.Contains(".png") )
-                        //    {
-                        //        flag++;
-                        //        client.DownloadFile(uri, $"{dir}\\Images\\Picture{flag}.png");
-                        //    }
-                        //    if (item.Contains(".svg"))
-                        //    {
-                        //        flag++;
-                        //        client.DownloadFile(uri, $"{dir}\\Images\\Picture{flag}.svg");                              
-                        //    }
-
-                        //}
-
-                        // //    if (item.Contains(".svg"))
-                        // //    { client1.DownloadFileAsync(uri, $"{dir}\\Images\\Picture{flag}.svg"); }
-                        // //    flag++;
-                        //    // Console.WriteLine(item);
-                        ////}
-
-                        //    if (item.Contains(".png"))
-                        //    {
-                        //        client1 = new WebClient();
-                        //        Uri uri = new Uri(path + item);
-                        //        client1.DownloadFileAsync(uri, $"{dir}\\Picture{flag}.png");
-                        //        flag++;
-                        //        Console.WriteLine(item);
-                        //    }
-
-                        //    if (item.Contains(".svg"))
-                        //    {
-                        //        client1 = new WebClient();
-                        //        Uri uri = new Uri(path + item);
-                        //        client1.DownloadFileAsync(uri, $"{dir}\\Picture{flag}.svg");
-                        //        flag++;
-                        //        Console.WriteLine(item);
-                        //    }
+                        if (item.Contains(".jpg"))
+                        {
+                            flag++;
+                            client.DownloadFile(uri, $"{dir}\\Images\\Picture{flag}.jpg");
+                            Console.WriteLine($"File {flag}: {item}");
+                        }
+                        if (item.Contains(".png"))
+                        {
+                            flag++;
+                            client.DownloadFile(uri, $"{dir}\\Images\\Picture{flag}.png");
+                            Console.WriteLine($"File {flag}: {item}");
+                        }
+                        if (item.Contains(".svg"))
+                        {
+                            flag++;
+                            client.DownloadFile(uri, $"{dir}\\Images\\Picture{flag}.svg");
+                            Console.WriteLine($"File {flag}: {item}");
+                        }
 
                     }
-                    Console.WriteLine( "There are {0} files in {1}", flag, path);
+
                 }
-            
+                Console.WriteLine("There are {0} files in {1}", flag, path);
+            }
+
             Console.ReadKey();
         }
 
